ac35eb69c8aae43ccf01a244d01986d522458631
ocsoriram <ossoriram@gmail.com>
2025-08-18 09:54:08 +0900

    feat: 盤面作成

---
+++ static/main.js
@@ -0,0 +1,56 @@
+const EMPTY = 0;
+const DARK = 1;
+const LIGHT = 2;
+
+let initialBoard = [];
+
+for (let col = 0; col < 8; col++) {
+  let boardRow = [];
+  for (let row = 0; row < 8; row++) {
+    boardRow.push(EMPTY);
+  }
+  initialBoard.push(boardRow);
+}
+
+// 初期stone配置
+initialBoard[3][3] = DARK;
+initialBoard[4][3] = LIGHT;
+initialBoard[3][4] = LIGHT;
+initialBoard[4][4] = DARK;
+
+const boardElement = document.querySelector("#board");
+
+const showBoard = async () => {
+  // 子要素が存在すれば全て削除する
+  while (boardElement.firstChild) {
+    boardElement.removeChild(boardElement.firstChild);
+  }
+  // 盤面を作成
+  initialBoard.forEach((line) => {
+    line.forEach((square) => {
+      // <div class="square">
+      const squareElement = document.createElement("div");
+      squareElement.className = "square";
+
+      // <div class="stone dark">
+      if (square !== EMPTY) {
+        const stoneElement = document.createElement("div");
+        const color = square === DARK ? "dark" : "light";
+        stoneElement.className = `stone ${color}`;
+
+        squareElement.appendChild(stoneElement);
+      }
+
+      boardElement.appendChild(squareElement);
+    });
+  });
+};
+
+const showStone = async (initialBoard) => {};
+
+const main = async () => {
+  await showBoard();
+  await showStone(initialBoard);
+};
+
+main();
